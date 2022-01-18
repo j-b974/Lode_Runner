a1ba005486b34fcad3b87242b484537a13523043
jb974 <bertil974@hotmail.fr>
2022-01-18 16:44:30 +0100

    ajoute sprite Animation

--- Game.js
+++ Game.js
@@ -1,12 +1,31 @@
+let lstSprite =[];
+
 function Game_load()
 {
+    let img = new Image();
+    img.src ="./img/vic.png";
 
+    let vic = new sprite(img)
+    vic.setAnimation(75, 74, false);
+    vic.addAnimation('runRight',[0,1,2,3],0.2 , true);
+    vic.addAnimation('grimp',[4,5], 0.2,true)
+    vic.addAnimation('runLeft', [8,9,10,11],0.2,true)
+    vic.StartAnimaiton('runLeft');
+
+    lstSprite.push(vic);
 }
 function Game_update(dt)
 {
+    lstSprite.forEach((sprite)=>
+    {
+        sprite.update(dt);
+    });
 
 }
 function Game_draw(ctx)
 {
-    
+    lstSprite.forEach((sprite)=>
+    {
+        sprite.draw(ctx);
+    })
 }
---
+++ sprite.js
@@ -0,0 +1,122 @@
+class sprite{
+
+    constructor(pimg)
+    {
+
+        this.img = pimg;
+        this.x = 50;
+        this.y = 50;
+        this.is_animate = false;
+        this.scall = 1;
+        this.setFrame = {
+            width: 0,
+            height:0
+
+        };
+        this.loop = false;
+        this.currentFrame = 3;
+        this.lstAnimation = [];
+        this.currentAnimation = null;
+        this.currentUpFrame = 0;
+        this.timer = 0;
+
+    }
+    setAnimation(pwidth , pheight )
+    {
+
+        this.setFrame.width = pwidth;
+        this.setFrame.height = pheight;
+       
+        this.is_animate = true;
+ 
+
+    }
+
+    addAnimation(pname , lstFrame , duration , loop)
+    {
+        let Animation= {
+            name:pname,
+            lstFrame:lstFrame,
+            duration:duration,
+            end:false,
+        }
+        this.loop = loop;
+
+        this.lstAnimation.push(Animation);
+
+    }
+
+    StartAnimaiton(pname)
+    {
+        if(this.currentAnimation != null && this.currentAnimation.name == pname && !this.currentAnimation.end)
+        {
+            return;
+        }
+        this.lstAnimation.forEach(animation =>{
+
+            if(animation.name == pname)
+            {
+                this.currentUpFrame = 0;
+                this.currentFrame = animation.lstFrame[this.currentUpFrame];
+
+                animation.end = false;
+                this.currentAnimation = animation;
+            }
+        })
+
+    }
+
+    
+
+    update(dt)
+    {
+
+        if(this.currentAnimation != null)
+        {
+            
+            this.timer += dt;
+            if(this.timer >= this.currentAnimation.duration)
+            {
+                this.timer = 0;
+                this.currentUpFrame++;
+
+                if(this.currentUpFrame >= this.currentAnimation.lstFrame.length)
+                {
+                    if(this.loop)
+                    {
+                        this.currentUpFrame = 0;
+
+                    }else{
+
+                        this.currentAnimation.end = true;
+                        this.currentUpFrame = this.currentAnimation.lstFrame.length-1;
+
+                    }
+                }
+                this.currentFrame = this.currentAnimation.lstFrame[this.currentUpFrame];
+            }
+
+        }
+    }
+    draw(ctx)
+    {
+        if(!this.is_animate)
+        {
+            ctx.drawImage(this.img , this.x , this.y);
+        }else{
+
+            let nbColumn = Math.floor(this.img.width/this.setFrame.width);
+
+            let row = Math.floor(this.currentFrame/nbColumn);
+
+            let col = this.currentFrame -(nbColumn*row);
+
+            let x = this.setFrame.width * col;
+
+            let y = this.setFrame.height * row ;
+
+
+            ctx.drawImage(this.img , x , y ,this.setFrame.width, this.setFrame.height , this.x ,this.y , this.setFrame.width*this.scall , this.setFrame.height*this.scall);
+        }
+    }
+}
